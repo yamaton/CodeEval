53fa14a06bb8ef049657dacc0c2bc8205dd0082d
Yamato Matsuoka <yamaton@gmail.com>
2013-07-19 22:02:43 -0400

    solved "lca.hs"

--- moderate/lca.hs
+++ moderate/lca.hs
@@ -1,5 +1,5 @@
 {-
-lca.hs
+Lowest Common Ancestor
 
 Created by Yamato Matsuoka on 2013-07-05.
 
@@ -41,13 +41,27 @@ Print to stdout, the least common ancestor, one per line. e.g.
 
 import System.Environment (getArgs)
 
+chain :: Int -> [Int]
+chain n = takeWhile (>0) $ iterate parent n 
+  where
+    parent 10 = 20
+    parent 29 = 20
+    parent 20 =  8
+    parent  3 =  8
+    parent  8 = 30
+    parent 52 = 30
+    parent _  =  0
+
+
 lowestCommonAncestor :: Int -> Int -> Int
-lowestCommonAncestor p q = 
+lowestCommonAncestor p q = head [x | x <- pChain, x `elem` qChain]
+  where [pChain, qChain] = map chain [p, q]
+
 
 main = do 
-    args <- getArgs
-    contents <- readFile (head args)
-    let inputs = [map read (words line) | line <- lines contents]
+    f:_ <- getArgs
+    contents <- readFile f
+    let inputs = [map read (words line) | line <- lines contents, (not . null) line]
     let outputs = [lowestCommonAncestor p q | [p, q] <- inputs]
-    mapM print outputs
+    mapM_ print outputs
 

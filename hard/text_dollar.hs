d349ebceae2479816094b06efac863854a49a40d
Yamato Matsuoka <yamaton@gmail.com>
2013-07-30 17:48:20 -0400

    linted

--- hard/text_dollar.hs
+++ hard/text_dollar.hs
@@ -39,33 +39,33 @@ import qualified Data.Map as Map
 textDollar :: Int -> String
 textDollar 1 = "OneDollar"
 textDollar n 
-    | millions > 0  = (textify millions) ++ "Million" ++ (textDollar (n `mod` 1000000))
-    | thousands > 0 = (textify thousands) ++ "Thousand" ++ (textDollar (n `mod` 1000))
-    | otherwise     = textify n ++ "Dollars"
-      where millions = n `div` 1000000
-            thousands = n `div` 1000
-            textify :: Int -> String
-            textify x 
-                | hundreds > 0 = (textOnes Map.! hundreds) ++ "Hundred" ++ (textify (x `mod` 100))
-                | tens > 0     = (textTens Map.! (10 * tens)) ++ (textify (x `mod` 10))
-                | ones > 0     = textOnes Map.! ones
-                | otherwise    = ""
-                  where hundreds  = x `div` 100
-                        (tens, ones) = if x < 20 
-                                           then (0, x) 
-                                           else (x `div` 10, x `mod` 10)
-                        textTens = Map.fromList [(20,"Twenty"), (30,"Thirty"), (40,"Forty"), (50,"Fifty"), 
-                                    (60,"Sixty"), (70,"Seventy"), (80,"Eighty"), (90,"Ninety")]
-                        textOnes = Map.fromList [(10,"Ten"), (11,"Eleven"), (12,"Twelve"), (13,"Thirteen"),
-                                    (14,"Fourteen"), (15,"Fifteen"), (16,"Sixteen"), (17,"Seventeen"),
-                                    (18,"Eighteen"), (19,"Nineteen"),
-                                    (1,"One"), (2,"Two"), (3,"Three"), (4,"Four"), (5,"Five"),
-                                    (6,"Six"), (7,"Seven"), (8,"Eight"), (9,"Nine")]
+  | millions > 0  = textify millions ++ "Million" ++ textDollar (n `mod` 1000000)
+  | thousands > 0 = textify thousands ++ "Thousand" ++ textDollar (n `mod` 1000)
+  | otherwise     = textify n ++ "Dollars"
+    where 
+      millions = n `div` 1000000
+      thousands = n `div` 1000
+      textify :: Int -> String
+      textify x 
+        | hundreds > 0 = textOnes Map.! hundreds ++ "Hundred" ++ textify (x `mod` 100)
+        | tens > 0     = textTens Map.! (10 * tens) ++ textify (x `mod` 10)
+        | ones > 0     = textOnes Map.! ones
+        | otherwise    = ""
+          where 
+            hundreds  = x `div` 100
+            (tens, ones) = if x < 20 then (0, x) else (x `div` 10, x `mod` 10)
+            textTens = Map.fromList [(20,"Twenty"), (30,"Thirty"), (40,"Forty"), (50,"Fifty"), 
+                        (60,"Sixty"), (70,"Seventy"), (80,"Eighty"), (90,"Ninety")]
+            textOnes = Map.fromList [(10,"Ten"), (11,"Eleven"), (12,"Twelve"), (13,"Thirteen"),
+                        (14,"Fourteen"), (15,"Fifteen"), (16,"Sixteen"), (17,"Seventeen"),
+                        (18,"Eighteen"), (19,"Nineteen"),
+                        (1,"One"), (2,"Two"), (3,"Three"), (4,"Four"), (5,"Five"),
+                        (6,"Six"), (7,"Seven"), (8,"Eight"), (9,"Nine")]
 
 
 main = do 
-    f:_ <- getArgs
-    contents <- readFile f
-    let inputs = map read $ filter (not . null) $ lines contents
-    let outputs = map textDollar inputs
-    mapM putStrLn outputs
+  f:_ <- getArgs
+  contents <- readFile f
+  let inputs = map read $ filter (not . null) $ lines contents
+  let outputs = map textDollar inputs
+  mapM putStrLn outputs

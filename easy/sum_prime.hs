219fe2f5e2458d3a76ffbd63409903eedb8509e4
Yamato Matsuoka <yamaton@gmail.com>
2013-07-31 00:47:05 -0400

    done "sum of primes"

--- easy/sum_prime.hs
+++ easy/sum_prime.hs
@@ -17,8 +17,36 @@ Your program should print the sum on stdout.i.e.
 
 -}
 
+import Control.Monad (when, forM_)
+import Data.Array.ST (newArray, readArray, writeArray, runSTUArray)
+import Data.Array.Unboxed (UArray, assocs)
 
-primes :: [Int]
 
-main = print $ sum (take 1000 primes)
+---- Eratosthenes sieve
+---- it can be more efficient by treating odd numbers only but it's good enough.
+sieve :: Int -> UArray Int Bool
+sieve n = runSTUArray $ do
+    let maxP = floor . sqrt $ fromIntegral n
+    sieveTF <- newArray (2, n) True 
+    forM_ [2..maxP] $ \p -> do
+      isPrime <- readArray sieveTF p
+      when isPrime $ do
+        forM_ [p*p, p*p+p .. n] $ \q -> do
+          writeArray sieveTF q False
+    return sieveTF
+
+
+-- Rosser's theorem is used to get an upper bound:
+-- For n-th prime number P(n), for n > 6
+-- log(n) + log(log(n)) - 1 < P(n)/n < log(n) + log(log(n))  
+-- http://en.wikipedia.org/wiki/Prime_number_theorem     
+primes :: Int -> [Int]
+primes n
+  | n < 6     = take n [2, 3, 5, 7, 11]
+  | otherwise = take n $ [i | (i,True) <- assocs $ sieve ub]
+    where 
+      x = fromIntegral n
+      ub = floor $ x * (log x + log (log x))
+
+main = (print . sum . primes) 1000
 

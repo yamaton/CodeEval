5da01be464f997ae591aa688bf810f46326e60bc
Yamato Matsuoka <yamaton@gmail.com>
2013-07-31 02:08:30 -0400

    done "count primes"

--- moderate/count_primes.hs
+++ moderate/count_primes.hs
@@ -22,44 +22,45 @@ Print out the number of primes between N and M (both inclusive)
 -}
 
 import System.Environment (getArgs)
+import Control.Monad (when, forM_)
+import Data.Array.ST (newArray, readArray, writeArray, runSTUArray)
+import Data.Array.Unboxed (UArray, assocs)
+
 
 split :: Char -> String -> [String]
 split c s = case dropWhile (== c) s of
-                "" -> []
-                s' -> w : split c s''
-                    where (w, s'') = break (== c) s'
-
--- | Eratosthenes Sieve
--- >>> primesTo 20
--- [2,3,5,7,11,13,17,19]
-primesTo :: Int -> [Bool]
-primesTo 2    = [2]
-primesTo n = 
-  | n < 2     = []
-  | otherwise = flip evalState initial $ do
-      forM 
-  
-  
-    initial = (take (n + 1) $ repeat True) :: [Bool]
-    maxP = floor . sqrt $ fromIntegral n
+  "" -> []
+  s' -> w : split c s''
+    where (w, s'') = break (== c) s'
 
 
+---- Eratosthenes sieve
+---- it can be more efficient by treating odd numbers only but it's good enough.
+sieve :: Int -> UArray Int Bool
+sieve n = runSTUArray $ do
+  let maxP = floor . sqrt $ fromIntegral n
+  sieveTF <- newArray (2, n) True 
+  forM_ [2..maxP] $ \p -> do
+    isPrime <- readArray sieveTF p
+    when isPrime $ do
+      forM_ [p*p, p*p+p .. n] $ \q -> do
+        writeArray sieveTF q False
+  return sieveTF
 
 
 primePi :: Int -> Int
-primePi = length . primesTo
+primePi n
+  | n < 2     = 0
+  | otherwise = length [i | (i,True) <- assocs $ sieve n]
+
 
 countPrimes :: Int -> Int -> Int
-countPrimes a b
-  | a > b     = 0
-  | a < 2     = primePi b
-  | otherwise = primePi b - (primePi (a - 1))
+countPrimes a b = primePi b - primePi (a - 1)
 
 
 main = do 
-    f:_ <- getArgs
-    contents <- readFile f
-    let inputs = [map read (split ',' line) | line <- lines contents]
-    let outputs = [countPrimes a b | [a, b] <- inputs ]
-    mapM_ print outputs
-
+  f:_ <- getArgs
+  contents <- readFile f
+  let inputs = [map read (split ',' line) | line <- lines contents]
+  let outputs = [countPrimes a b | [a, b] <- inputs ]
+  mapM_ print outputs
